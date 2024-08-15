2242b576a9a1aafdf2f1ac6852665aa3fddaed20
Miguel Leonar <migsldev@gmail.com>
2024-08-15 15:22:24 +1000

    created routes file

---
+++ app/routes.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from app.models import User, Property, Application, Wishlist
+from app.schemas import UserSchema, PropertySchema, ApplicationSchema, WishlistSchema
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
+main = Blueprint('main', __name__)
+
+user_schema = UserSchema()
+property_schema = PropertySchema()
+application_schema = ApplicationSchema()
+wishlist_schema = WishlistSchema()
+
+@main.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = generate_password_hash(data.get('password'))
+    role = data.get('role')
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"message": "User already exists"}), 400
+
+    new_user = User(username=username, email=email, password=password, role=role)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user), 201
+
+# User Login
+@main.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'role': user.role})
+    return jsonify({"access_token": access_token}), 200
+
